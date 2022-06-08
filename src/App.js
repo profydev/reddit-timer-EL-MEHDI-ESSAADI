744dc136f740add6980a4b5898b3cdc6d055bf5c
El Mehdi Essaadi <gsio1487@gmail.com>
2022-06-08 15:11:48 +0100

    fix lint errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "index.scss";
-import { SharedLayout } from "Pages";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.scss';
+import { SharedLayout } from './Pages';
 
 function App() {
-   return (
-      <BrowserRouter>
-         <Routes>
-            <Route path="/" element={<SharedLayout />}>
-               <Route index />
-               <Route path="search" />
-            </Route>
-         </Routes>
-      </BrowserRouter>
-   );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index />
+          <Route path="search" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
--- src/Pages/SharedLayout.js
+++ src/Pages/SharedLayout.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 function SharedLayout() {
-   return <div>SharedLayout</div>;
+  return <div>SharedLayout</div>;
 }
 
 export default SharedLayout;
