92d8c4decab83190bf3df20c002486d836370011
El Mehdi Essaadi <gsio1487@gmail.com>
2022-06-08 14:32:33 +0100

    finish create app seleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "index.scss";
+import { SharedLayout } from "Pages";
 
 function App() {
-  return (
-    <div>
-      App Placeholder
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<SharedLayout />}>
+               <Route index />
+               <Route path="search" />
+            </Route>
+         </Routes>
+      </BrowserRouter>
+   );
 }
-
 export default App;
